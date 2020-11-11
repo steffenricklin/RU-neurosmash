0a160a3a9a6cc341fd3762e029477817cb0d4831
augub <augustijndeboer@gmail.com>
2020-11-11 17:18:20 +0100

    Added some links to documentation

--- RNN/rnn_model.py
+++ RNN/rnn_model.py
@@ -1,7 +1,10 @@
-import mxnet
-import numpy as np
+from mxnet import nd            # This is for the array types. Use nd.array.  Similar to numpy by design 
+                                # https://mxnet.apache.org/versions/1.7.0/api/python/docs/api/ndarray/index.html
 
-class RNN_GMN:
+from mxnet.gluon import nn      # This is for the neural network functionality
+                                # https://mxnet.apache.org/versions/1.7.0/api/python/docs/api/gluon/index.html
+
+class RNN_GMN(nn.Block):
     def __init__(self, a):
         self.a = a
         pass
