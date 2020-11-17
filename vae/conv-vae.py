fc0904214d6e6952a7f23ebe521a624790406bdb
Steffen <steffenricklin@gmail.com>
2020-11-17 08:06:36 +0000

    reconstruction implemented. Decoding still open

--- vae/conv-vae.py
+++ vae/conv-vae.py
@@ -33,37 +33,42 @@ class ConvVae(gluon.HybridBlock):
         # Input has the shape size x size x 3
         encoder = nn.HybridSequential(prefix='encoder')
         # relu conv 32 x 4
-        encoder.add(nn.Conv2D(32*4, activation='relu'))
+        encoder.add(nn.Conv2D(32*4, strides=2, activation='relu'))
         # relu conv 64 x 4
-        encoder.add(nn.Conv2D(64*4, activation='relu'))
+        encoder.add(nn.Conv2D(64*4, strides=2, activation='relu'))
         # relu conv 128 x 4
-        encoder.add(nn.Conv2D(128*4, activation='relu'))
+        encoder.add(nn.Conv2D(128*4, strides=2, activation='relu'))
         # relu conv 256 x 4
-        encoder.add(nn.Conv2D(256*4, activation='relu'))
+        encoder.add(nn.Conv2D(256*4, strides=2, activation='relu'))
 
         return encoder
         
     def get_vae():
+        
         pass
 
     def getDecoder(self):
         # shape of z should be already reshape from (1024x1x1)  to 1x1x1024
         decoder = nn.HybridSequential(prefix='decoder')
         # relu deconv 128x5 to 5x5x128
-        decoder.add(nn.Conv2DTranspose((128,5), strides=2, activation='relu'))
+        decoder.add(nn.Conv2DTranspose(128*5, strides=2, activation='relu'))
         # relu deconv 64x5 to 13x13x64
-        decoder.add(nn.Conv2DTranspose((64,5), strides=2, activation='relu'))
+        decoder.add(nn.Conv2DTranspose(64*5, strides=2, activation='relu'))
         # relu deconv 32x6 to 30x30x32
-        decoder.add(nn.Conv2DTranspose((32,6), strides=2, activation='relu'))
+        decoder.add(nn.Conv2DTranspose(32*6, strides=2, activation='relu'))
         # sigmoid deconv 3x6 to 64x64x3
-        decoder.add(nn.Conv2DTranspose((128,5), strides=2, activation='sigmoid'))
+        decoder.add(nn.Conv2DTranspose(128*5, strides=2, activation='sigmoid'))
 
         return decoder
 
-    def hybrid_forward(self, F, x, *args, **kwargs):
+    def hybrid_forward(self, F, x, train=False, *args, **kwargs):
         h = self.encoder(x)
 
         # generate z from h
         z = self.get_vae(h)
-        
-        pass
+
+        if not train:
+            return z  # return z
+        else: # reconstruct
+            z = z.reshape((1, 1, 1024))
+            recon = self.decoder(z)
