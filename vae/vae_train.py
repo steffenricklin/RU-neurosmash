3988d96586cbafdebb988bca3c080cb8b5cc6ae6
CodeRonNor <ronh93@gmail.com>
2020-12-04 15:55:53 +0100

    online vae

---
+++ vae/vae_train.py
@@ -0,0 +1,175 @@
+
+# Imports
+from convvae import ConvVae
+import numpy as np
+import PIL
+import mxnet as mx
+from mxnet import nd, autograd, gluon
+from mxnet.gluon import nn
+from sklearn.model_selection import train_test_split
+from tqdm.notebook import tqdm as tqdm_notebook
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import Neurosmash
+
+ip         = "127.0.0.1"
+port       = 13000
+size       = 128  # 96, 192
+timescale  = 1
+agent = Neurosmash.Agent()
+environment = Neurosmash.Environment(ip, port, size, timescale)
+
+# end (true if the episode has ended, false otherwise)
+# reward (10 if won, 0 otherwise)
+# state (flattened size x size x 3 vector of pixel values)
+# The state can be converted into an image as follows:
+# image = np.array(state, "uint8").reshape(size, size, 3)
+# You can also use to Neurosmash.Environment.state2image(state) function which returns
+# the state as a PIL image
+
+shape = (3, size, size)
+
+
+def roam_and_collect(nr_images=10):
+
+    # The following steps through an entire episode from start to finish with random actions (by default)
+    images = np.zeros((nr_images, *shape))
+
+    # Do as many rounds as we need to collect the desired number of images
+    i = 0
+    while i < nr_images:
+        end, reward, state = environment.reset()
+        # While no one is dead, roam the environment and collect images
+        while end == 0:
+            action = agent.step(end, reward, state)
+            end, reward, state = environment.step(action)
+            images[i] = np.array(state).reshape(shape).astype(np.uint8)
+            i += 1
+            if i >= nr_images:
+                end = 1
+
+    return images
+
+
+def plot_samples(images, n_samples=10):
+    idx = np.random.choice(images.shape[0], n_samples)
+    _, axarr = plt.subplots(1, n_samples, figsize=(16, 4))
+    for i, j in enumerate(idx):
+        axarr[i].imshow(images[j].reshape(shape[1], shape[2], shape[0]).astype(np.uint8))
+        axarr[i].get_xaxis().set_ticks([])
+        axarr[i].get_yaxis().set_ticks([])
+    plt.show()
+
+
+def initialise_vae(data, batch_size=10, z_size=32):
+
+    # Split data
+    train_data, test_data = train_test_split(data, test_size=0.33, random_state=42)  # change random state to get different distribution
+    train_iter = mx.io.NDArrayIter(data={'data': train_data}, batch_size=batch_size)
+    test_iter = mx.io.NDArrayIter(data={'data': test_data}, batch_size=batch_size)
+
+    # Create model
+    vae = ConvVae(batch_size=batch_size, size=size, z_size=z_size)
+    vae.collect_params().initialize(mx.init.Xavier(), ctx=vae.ctx)
+    vae.hybridize()  # activates hybrid-mode of the Hybrid-Block
+    trainer = gluon.Trainer(vae.collect_params(), 'adam', {'learning_rate': .001})
+
+    return vae, train_iter, test_iter, trainer
+
+
+def train_vae(vae, train_iter, test_iter, trainer, n_epochs=50, print_period=10):
+    training_loss = []
+    validation_loss = []
+    outputs = np.zeros((1, *shape))
+
+    for epoch in tqdm(range(n_epochs), desc='epochs'):
+
+        # Reset vars
+        epoch_loss = 0
+        train_iter.reset()
+        test_iter.reset()
+
+        # Training data
+        n_batch_train = 0
+        for batch in train_iter:
+            n_batch_train += 1
+            data = batch.data.pop().as_in_context(vae.ctx)
+            with autograd.record():
+                out, loss = vae(data)
+            loss.backward()
+            trainer.step(data.shape[0])
+            epoch_loss += nd.mean(loss).asscalar()
+
+            # See reconstruction
+            plt.imshow(np.reshape(out[0].asnumpy(), (size, size, 3)))
+            plt.show()
+
+        # Validation data
+        n_batch_val = 0
+        epoch_val_loss = 0
+        for batch in test_iter:
+            n_batch_val += 1
+            data = batch.data.pop().as_in_context(vae.ctx)
+            out, loss = vae(data)
+            epoch_val_loss += nd.mean(loss).asscalar()
+            outputs = np.concatenate((outputs, out.asnumpy()), axis=0)
+
+        epoch_loss /= n_batch_train
+        epoch_val_loss /= n_batch_val
+
+        training_loss.append(epoch_loss)
+        validation_loss.append(epoch_val_loss)
+
+        if epoch % max(print_period, 1) == 0:
+            tqdm.write(
+                'Epoch{}, Training loss {:.2f}, Validation loss {:.2f}'.format(epoch, epoch_loss, epoch_val_loss))
+    print("done.")
+
+    return training_loss, validation_loss, outputs
+
+
+def plot_reconstruction(out):
+    output = np.reshape(out, (size, size, 3))
+    plt.imshow(output)
+    plt.show()
+
+
+def plot_loss(n_epochs, training_loss, validation_loss):
+    batch_x = np.linspace(1, n_epochs, len(training_loss))
+    plt.plot(batch_x, np.array(training_loss))
+    plt.plot(batch_x, np.array(validation_loss))
+    plt.legend(['train', 'valid'])
+    plt.show()
+
+
+def compare_io(images, outputs):
+    # Original
+    plt.figure()
+    plt.imshow(images[-1].reshape(shape[1], shape[2], shape[0]).astype(np.uint8))
+    plt.title('Original')
+    plt.show()
+
+    # Reconstruction
+    # rnd = np.random.randint(outputs.shape[0])
+    plot_reconstruction(outputs[-1])
+
+
+def run(nr_images=100, n_epochs=10):
+    images = roam_and_collect(nr_images=nr_images)
+    print('got images...')
+    plot_samples(images, n_samples=10)
+    print('plotted samples...')
+    # map images to the range [0, 1]
+    data = np.interp(images, [0, 255], [0.0, 1.0])
+    vae, train_iter, test_iter, trainer = initialise_vae(data, batch_size=10, z_size=32)
+    print('vae initialised...')
+    training_loss, validation_loss, outputs = train_vae(vae, train_iter, test_iter, trainer, n_epochs, print_period=1)
+    print('vae trained...')
+    plot_loss(n_epochs, training_loss, validation_loss)
+    print('loss plotted...')
+    compare_io(images, outputs)
+    print('comparison done.')
+
+
+if __name__ == '__main__':
+    run(250, 50)
---
+++ vae/vae_trainer.py
@@ -0,0 +1,204 @@
+
+# Imports
+from convvae import ConvVae
+import numpy as np
+import PIL
+import mxnet as mx
+from mxnet import nd, autograd, gluon
+from mxnet.gluon import nn
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import Neurosmash
+
+ip         = "127.0.0.1"
+port       = 13000
+size       = 128  # 96, 192
+timescale  = 1
+agent = Neurosmash.Agent()
+environment = Neurosmash.Environment(ip, port, size, timescale)
+
+# end (true if the episode has ended, false otherwise)
+# reward (10 if won, 0 otherwise)
+# state (flattened size x size x 3 vector of pixel values)
+# The state can be converted into an image as follows:
+# image = np.array(state, "uint8").reshape(size, size, 3)
+# You can also use to Neurosmash.Environment.state2image(state) function which returns
+# the state as a PIL image
+
+
+def initialise_vae(batch_size=10, z_size=32):
+
+    # Create model
+    vae = ConvVae(batch_size=batch_size, size=size, z_size=z_size)
+
+    # Initialise weights
+    vae.collect_params().initialize(mx.init.Xavier(), ctx=vae.ctx)
+
+    # Activates hybrid-mode of the Hybrid-Block
+    vae.hybridize()
+
+    # Initialise trainer
+    trainer = gluon.Trainer(vae.collect_params(), 'adam', {'learning_rate': .001})
+
+    return vae, trainer
+
+def state2image(state):
+    return np.array(state).reshape(orig_shape).astype(np.uint8)
+
+def state2input(state):
+    inpt = np.reshape(state, (1, *tens_shape)).astype(np.uint8)
+    inpt = np.interp(inpt, [0, 255], [0.0, 1.0])
+    inpt = mx.nd.array(inpt)
+    return inpt
+
+def output2state(output):
+    return np.reshape(output.asnumpy(), (1, *orig_shape))[0]
+
+def train_vae(vae, trainer, n_epochs=50, print_period=10, n_images_train=100, n_images_valid=100):
+
+    training_loss = []
+    validation_loss = []
+
+    ### Training
+    print('Starting training...')
+    i = 0
+    while i < n_images_train:
+        print('New battle in training mode...')
+        end, reward, state = environment.reset()
+
+        while not end:
+
+            # Find an action
+            action = agent.step(end, reward, state)
+
+            # Take a step; get a new state
+            end, reward, state_next = environment.step(action)
+
+            # If nothing changes anymore, break
+            if state == state_next:
+                print('resetting environment')
+                break
+
+            # Save next state
+            state = state_next
+
+            # show input
+            # plt.imshow(state2image(state))
+            # plt.show()
+
+            # Train on one image
+            data = state2input(state)
+            epoch_loss = 0
+            for _ in range(n_epochs):
+                with autograd.record():
+                    out, loss = vae(data)
+                loss.backward()
+                trainer.step(1)  # batch size = 1
+                epoch_loss += loss.asscalar()
+                print(epoch_loss)
+
+            training_loss.append(epoch_loss/n_epochs)
+
+            print(f'training round {i} with loss {epoch_loss/n_epochs}')
+
+
+            # Plot training reconstruction
+            # stateagain = output2state(out)
+            # plt.imshow(stateagain)
+            # plt.show()
+
+            # Stop if we have the desired amount of images
+            i += 1
+            if i >= n_images_train:
+                end = 1
+
+
+    ### Validation
+    print('Starting validation...')
+
+    i = 0
+    while i < n_images_valid:
+        print('New battle in validation mode...')
+        end, reward, state = environment.reset()
+
+        while not end:
+
+            # Find an action
+            action = agent.step(end, reward, state)
+
+            # Take a step; get a new state
+            end, reward, state_next = environment.step(action)
+
+            # If nothing changes anymore, presumably when an agent has died, break
+            if state == state_next:
+                print('resetting environment')
+                break
+
+            # Save next state
+            state = state_next
+
+            data = state2input(state)
+
+            epoch_val_loss = 0
+            for _ in range(n_epochs):
+                out, loss = vae(data)
+                print(f'validation loss: {loss}')
+                epoch_val_loss += loss.asscalar()
+            validation_loss.append(epoch_val_loss/n_epochs)
+
+
+
+            # Plot validation reconstruction
+            print(f'validation reconstruction after {i} images')
+            stateagain = output2state(out)
+            plt.imshow(stateagain)
+            plt.show()
+
+            # Stop if we have the desired amount of images
+            i += 1
+            if i >= n_images_train:
+                end = 1
+
+    return training_loss, validation_loss
+
+
+def plot_reconstruction(out):
+    output = np.reshape(out, (size, size, 3))
+    plt.imshow(output)
+    plt.show()
+
+
+def plot_loss(n_epochs, training_loss, validation_loss):
+    # batch_x = np.linspace(1, n_epochs, len(training_loss))
+    # plt.plot(batch_x, np.array(training_loss))
+    # plt.plot(batch_x, np.array(validation_loss))
+    plt.plot(training_loss)
+    plt.plot(validation_loss)
+    plt.legend(['train', 'valid'])
+    plt.show()
+
+
+def run(n_images_train=100, n_images_valid=100, n_epochs=10):
+
+    # Initialise the VAE
+    vae, trainer = initialise_vae(batch_size=1, z_size=32)
+    print('VAE initialised...')
+
+    # Train the VAE
+    training_loss, validation_loss = train_vae(
+        vae, trainer, n_epochs, print_period=1, n_images_train=n_images_train, n_images_valid=n_images_valid
+    )
+    print('vae trained...')
+
+    plot_loss(n_epochs, training_loss, validation_loss)
+    # print('loss plotted...')
+
+    # compare_io(images, outputs)
+    # print('comparison done.')
+
+
+orig_shape = (size, size, 3)
+tens_shape = (3, size, size)
+if __name__ == '__main__':
+    run(n_images_train=250, n_images_valid=100, n_epochs=4)
