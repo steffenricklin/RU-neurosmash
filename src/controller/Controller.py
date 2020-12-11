98049001d537813a774310b9e73c85615ac95cc7
David <leeft047@planet.nl>
2020-12-12 00:01:02 +0100

    Added updated controller files from RolloutGenerator branch

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -1,33 +1,44 @@
-import pickle
 import numpy as np
-from settings import *
 np.random.seed(2020)
+from settings import *
 
+class Controller():
 
-class Controller:
+    def __init__(self, weights=None, n_actions = 3):
+        self.n_actions = n_actions
 
-    def __init__(self, weights=np.random.normal(0,1, z_dim+h_dim)):
-        self.weights = weights
+        if weights is not None:
+            self.weights = weights
+        else:
+            self.weights = np.random.normal(0, 1, (n_actions, z_dim + h_dim))
 
-    def action(self, S, theta=0):
+    def action(self, z, h, theta=0):
         """
         Get an action from the controller
         Computes a_t = W_c [z_t h_t] + b_t
         
-        :todo this function has not been tested 
-        :todo biases are not yet included 
+        :todo biases are not yet included
+        :todo This should be four actions
         
-        :param S (vector) z_t appended with h : latent state at timepoint t + hidden state at timepoint t
+        :param z (vector) z_t: latent state at timepoint t + hidden state at timepoint t
+        :param h (vector) h_t: hidden state at timepoint t
         :param theta (float) decision threshold for action
         
         :return action (int) clipped with tanh()
         """
+        S = np.append(z, h)
         a = self.weights @ S #+ self.bias
-        a = np.tanh(a)
-        return 1 if a>theta else -1
+        a = np.exp(a)/np.exp(a)
+        return a.argmax()
 
-    def load_parameters(self, filepath):
-        self.weights = pickle.load(filepath)
+    def get_weight_array(self):
+        """
+        :return: weights as a matrix of lenght h_dim + z_dim
+        """
+        return self.weights.flatten()
 
-    def save_parameters(self, filepath):
-        pickle.dump(self.weights, path_to_ctrl_params)
+    def set_weight_array(self, weights):
+        """:param weights (h_dim+z_dim,)
+        Reshapes the weights to a matrix form
+        """
+        self.weights = weights.reshape(self.n_actions, int(len(weights)/4))
--- src/controller/ES_trainer.py
+++ src/controller/ES_trainer.py
@@ -1,15 +1,13 @@
 import numpy as np
-
 np.random.seed(2020)
 from multiprocessing import Pool
 from tqdm import tqdm
 import time
-# from utils.rollout import RolloutGenerator
+from utils.rollout import RolloutGenerator
 from controller.Controller import *
 
-
 class ES_trainer():
-
+    
     def __init__(self, rollout_generator, pop_size, elite_size):
         """
         Gaussian Evolution Strategy algorithm (without covariances)
@@ -20,50 +18,52 @@ class ES_trainer():
         self.elite_size = elite_size
         self.pop_size = pop_size
         self.w_dim = z_dim + h_dim
-        self.weights = np.random.normal(0, 1, self.w_dim)
+        self.weights = np.random.normal(0,1,self.w_dim)
         self.sigma = 1
         self.loss_func = rollout_generator.rollout
-
+        
     def train(self, n_iter, parallel=False):
         """
         :param n_iter    (int) number of iterations
+
         :return trained controller (Controller) trained controller object with trained weights
         :return fitness  (matrix) fitness score for each member for each iteration
         """
         pop_size, elite_size = self.pop_size, self.elite_size
-        w, sigma = self.weights, self.sigma
-        reward = np.zeros((n_iter, pop_size + 1))
-
+        w, sigma  = self.weights, self.sigma
+        reward = np.zeros((n_iter, pop_size+1))
+ 
         tic = time.perf_counter()
         for i in tqdm(range(n_iter)):
             # Generate K population members
-            population = np.random.multivariate_normal(mean=w, cov=sigma * np.eye(self.w_dim), size=pop_size)
+            population = np.random.multivariate_normal(mean=w, cov=sigma*np.eye(self.w_dim), size=pop_size)
             controllers = [Controller(weights) for weights in population]
-
+            
             # Multiprocess each population member
             if parallel:
                 with Pool(os.cpu_count()) as pool:
-                    fitness = pool.map(self.loss_func, controllers)  # use starmap() for multiple argument functions.
-                reward[i, :] = np.append(self.loss_func(Controller(w)),
-                                         fitness)  # First entry is of mean weights, after that of pop. members
+                    fitness = pool.map(self.loss_func, controllers) # use starmap() for multiple argument functions.
+                reward[i,:] = np.append(self.loss_func(Controller(w)), fitness) # First entry is of mean weights, after that of pop. members
             # or sequential training
             else:
                 fitness = np.zeros(pop_size)
                 for i in range(pop_size):
                     fitness[i] = self.loss_func(controllers[i])
-                reward[i, :] = np.append(self.loss_func(Controller(w)), fitness)
+                reward[i,:] = np.append(self.loss_func(Controller(w)), fitness)
 
-            # Sort population and take elite
-            elite_idx = np.argsort(fitness)[:elite_size]
-            elite = population[elite_idx]
+            # Sort population and take elite            
+            elite_idx  = np.argsort(fitness)[:elite_size]
+            elite      = population[elite_idx]
 
             # Update w and Σ using elite
             w = np.mean(elite, axis=0)
-            sigma = np.sum((elite - w) ** 2) / elite_size
+            sigma = np.sum((elite-w)**2) / elite_size
+
 
         toc = time.perf_counter()
         print(f'Duration of training the controller: {toc - tic:0.4f} seconds')
-
+        
         self.weights = w
         self.sigma = sigma
         return Controller(w), reward
+    
